7ba959ff3f0ab69447c8653e7f8e86d67f56b1ce
solembum-php <solembum@sole.com>
2019-10-28 21:05:15 +0200

    change autoload and add function for root path

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,8 +1,16 @@
 <?php
 
+/**
+ * 
+ * @return string app path from server root
+ */
+function getAppPath() {
+    return $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR;
+}
+
 spl_autoload_register(function($class) {
     $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-    $classPath = $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . $class . '.php';
+    $classPath = getAppPath() . $class . '.php';
     if (file_exists($classPath)) {
 	include_once $classPath;
 	return true;
