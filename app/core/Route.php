89becfed0d296597e5f4c488afbc7b032b111ff6
solembum-php <solembum@sole.com>
2019-10-28 20:29:49 +0200

    routing

--- app/core/Route.php
+++ app/core/Route.php
@@ -1,9 +1,62 @@
 <?php
 
 namespace core;
+
 class Route {
-    static public function init(){
-	//TODO create base routing
-	echo 'the site is working';
+
+    /**
+     * routing/controller/action
+     */
+    
+    static public function init() {
+	//######################################################################
+	//получить дополнительный путь и разбить его на элементы по / если есть 
+	//лишние элементы, то выдать 404. Если нет лишних, то первая часть - имя
+	//контроллера(класса), вторая часть - имя экшна(метода). Если элементов 
+	//меньше 2х, то экшн - index, контроллер main.Если указанного класса 
+	//контроллера не существует, или метода экшн, то 404. сделать метод 
+	//статический для ошибки 404
+	//######################################################################
+	//это значение останется в переменной, если не произойдет перезапись
+	$controllerName = 'main'; 
+	//это значение останется в переменной, если не произойдет перезапись
+	$actionName = 'index';
+	//забираем дополнительный путь документа в домене и разделяем его на список(это массив с числовым индексом)
+	$routeItems = explode('/', $_SERVER['REQUEST_URI']); 
+	//0-й элемент всегда содержит пустую строку поэтому удаляем его
+	array_shift($routeItems); 
+	//проверяем последний элемент массва на пустое значение
+	//при необходимости удаляем его
+	if (empty($routeItems[count($routeItems) - 1])) {
+	    array_pop($routeItems);
+	}
+	//если в массиве больше двух элементов, то 404
+	if (count($routeItems) > 2) {
+	    self::error404();
+	}
+	if (!empty($routeItems[0])) {
+	    //для кириллицы
+	    //$controllerName = mb_strtolower(urldecode($routeItems[0]));
+	    $controllerName = strtolower($routeItems[0]);
+	}
+	if (!empty($routeItems[1])) {
+	    //$actionName = mb_strtolower(urldecode($routeItems[1]));//для кириллицы
+	    $actionName = strtolower($routeItems[1]);
+	}
+	//$controllerClassName = mb_convert_case($controllerName, MB_CASE_TITLE);// для кириллицы
+	$controllerClassName = 'controllers\\' . ucfirst($controllerName) . 'Controller';
+	if (!class_exists($controllerClassName)) {
+	    self::error404();
+	}
+	$controller = new $controllerClassName();
+	if (!method_exists($controller, $actionName)) {
+	    self::error404();
+	}
+	$controller->$actionName();
     }
+
+    static public function error404() {
+	exit('404');
+    }
+
 }
