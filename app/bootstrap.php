6ee1338c196d644dfcf0a4b0b3032b12c9f6e299
solembum-php <solembum@sole.com>
2019-10-30 19:01:35 +0200

    add function to get full url

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -8,6 +8,21 @@ function getAppPath() {
     return $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR;
 }
 
+/**
+ * Возвращает указанный путь на текущем сайте
+ * 
+ * @param string $path дополнительный путь сайта 
+ * - то что после основного домена
+ */
+function url(string $path = null) {
+    if($_SERVER['SERVER_PORT'] === '80'){
+	$scheme = 'http';
+    }else if($_SERVER['SERVER_PORT'] === '443'){
+	$scheme = 'https';
+    }
+    return $scheme . '://' . $_SERVER['HTTP_HOST'] . $path;
+}
+
 spl_autoload_register(function($class) {
     $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
     $classPath = getAppPath() . $class . '.php';
