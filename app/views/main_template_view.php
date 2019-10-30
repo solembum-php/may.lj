125af384bab3499796e922dba713a266c31a0a9b
solembum-php <solembum@sole.com>
2019-10-30 19:11:39 +0200

    created main template

--- app/views/main_template_view.php
+++ app/views/main_template_view.php
@@ -5,6 +5,20 @@
         <title></title>
     </head>
     <body>
-	lol
+	<header>
+	    <h1>
+		<a href="<?= url('/') ?>">SiteName</a>
+	    </h1>
+	</header>
+	<nav>
+	    <ul>
+		<li><a href="<?= url('/') ?>">Home</a></li>
+		<li><a href="<?= url('/auth') ?>">Log in</a></li>
+	    </ul>
+	</nav>
+	<main>
+	    <?php include_once getAppPath() . 'views' . DIRECTORY_SEPARATOR . $page . '.php'; ?>
+	</main>
+	<footer>"may" webstudio 2019$copy;</footer>
     </body>
 </html>
