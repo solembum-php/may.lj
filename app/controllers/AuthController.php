0f05572b37293a285401b8e4606659c36cad7843
solembum-php <solembum@i.ua>
2019-11-01 13:15:23 +0200

    added to AuthController

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -10,29 +10,42 @@ use models\AuthModel;
 class AuthController extends AbstractController {
 
     public function index() {
-	$this->view->render('auth_index_view');
+        $this->view->render('auth_index_view');
     }
 
     public function registration() {
-	$this->view->render('auth_register_view');
+        $this->view->render('auth_register_view');
     }
 
     public function regproc() {
-	if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-	    die('not available request method');
-	}
-	$user = filter_input_array(INPUT_POST);
-	//TODO все проверки
-	$model = $this->getModel();
-	$model->addUser($user);
-	Route::redirect(url('/auth'));
+        if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+            die('not available request method');
+        }
+        
+        $user = filter_input_array(INPUT_POST);
+        $errors = Route::userValidate($user);
+
+        if (!empty($errors)) {
+            
+            $_SESSION['errors'] = $errors;
+            $_SESSION['login'] = $user['login'];
+            $_SESSION['email'] = $user['email'];
+            
+            var_dump($_SESSION);
+            Route::redirect(url('/auth/registration'));
+        } else {
+            $model = $this->getModel();
+            $model->addUser($user);
+            Route::redirect(url('/auth'));
+        }
+        //TODO все проверки
     }
-    
-    protected function getModel(){
-	if(!$this->model){
-	    $this->model = new AuthModel();
-	}
-	return $this->model;
+
+    protected function getModel() {
+        if (!$this->model) {
+            $this->model = new AuthModel();
+        }
+        return $this->model;
     }
 
 }
