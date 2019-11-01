46acb4170a541fffbaeb4c0730c9a6c7ac0e0d4f
solembum-php <solembum@i.ua>
2019-11-01 13:17:10 +0200

    saved previous version of main_template_view.php

--- app/views/main_template_view.php
+++ app/views/main_template_view.php
@@ -3,23 +3,23 @@
     <head>
         <meta charset="UTF-8">
         <title></title>
-	<link rel="stylesheet" href="/css/main.css"/>
+        <link rel="stylesheet" href="/css/main.css"/>
     </head>
     <body>
-	<header>
-	    <h1>
-		<a href="<?= url('/') ?>">SiteName</a>
-	    </h1>
-	</header>
-	<nav>
-	    <ul>
-		<li><a href="<?= url('/') ?>">Home</a></li>
-		<li><a href="<?= url('/auth') ?>">Log in</a></li>
-	    </ul>
-	</nav>
-	<main>
-	    <?php include_once getAppPath() . 'views' . DIRECTORY_SEPARATOR . $page . '.php'; ?>
-	</main>
-	<footer>"may" webstudio 2019&copy;</footer>
+        <header>
+            <h1>
+                <a href="<?= url('/') ?>">SiteName</a>
+            </h1>
+        </header>
+        <nav>
+            <ul>
+                <li><a href="<?= url('/') ?>">Home</a></li>
+                <li><a href="<?= url('/auth') ?>">Log in</a></li>
+            </ul>
+        </nav>
+        <main>
+            <?php include_once getAppPath() . 'views' . DIRECTORY_SEPARATOR . $page . '.php'; ?>
+        </main>
+        <footer>"may" webstudio 2019&copy;</footer>
     </body>
 </html>
