dca7ffd5677bac6778b7a881dd1aa5ba4869c3cb
solembum-php <solembum@i.ua>
2019-11-01 13:15:54 +0200

    added to auth_register_view.php

--- app/views/auth_register_view.php
+++ app/views/auth_register_view.php
@@ -1,16 +1,34 @@
+<?php
+$errors = core\Route::getErrors();
+?>
 <form action="<?= url('/auth/regproc') ?>" method="post">
+    <ul>
+        <?php
+        
+        if (count($errors) !== 0) {
+            foreach ($errors as $error) {
+                echo '<li>' . $error . '</li>';
+            }
+        }
+        ?>
+    </ul>
     <label>Login:
-	<input type="text" name="login"/>
+        <input type="text" name="login" value="<?= $_SESSION['login'] ?>"/>
     </label>
     <label>Password:
-	<input type="password" name="pass"/>
+        <input type="password" name="pass"/>
     </label>
     <label>Confirm password:
-	<input type="password" name="pass_conf"/>
+        <input type="password" name="pass_conf"/>
     </label>
     <label>Email:
-	<input type="email" name="email"/>
+        <input type="email" name="email" value="<?= $_SESSION['email'] ?>"/>
     </label>
     <input type="submit" value="зарегистрироваться"/>
 </form>
+<?php
+//чтобы не сохранились к следующему запуску
+$_SESSION['errors'] = null;
+$_SESSION['login'] = null;
+$_SESSION['email'] = null;
 
