24d36a20274c1bd252d7bc5106050d971167d5c8
solembum-php <solembum@sole.com>
2019-11-04 20:34:31 +0200

    show posts

--- app/views/posts_index_view.php
+++ app/views/posts_index_view.php
@@ -1,2 +1,7 @@
-lol
+<?php foreach ($this->posts as $postItem) : ?>
+    <h2><a href="<?= url('/posts/item?id' . $postItem['title']) ?>"><?= $postItem['title'] ?></a></h2>
+    <div><?= $postItem['text'] ?></div>
+
+    <div>Автор: <?= $postItem['title'] ?></div>
+<?php endforeach; ?>
 
