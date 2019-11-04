4b850fcb5551f9c51a9639d9bbe5b8b4aab52920
solembum-php <solembum@i.ua>
2019-11-04 12:20:33 +0200

    add to main_template_view.php

--- app/views/main_template_view.php
+++ app/views/main_template_view.php
@@ -4,6 +4,8 @@
         <meta charset="UTF-8">
         <title></title>
         <link rel="stylesheet" href="/css/main.css"/>
+        <script src="https://cdn.ckeditor.com/ckeditor5/12.4.0/classic/ckeditor.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     </head>
     <body>
         <header>
@@ -14,7 +16,7 @@
         <nav>
             <ul>
                 <li><a href="<?= url('/') ?>">Home</a></li>
-		<li><a href="<?= url('/news') ?>">News</a></li>
+		<li><a href="<?= url('/news/index') ?>">News</a></li>
 		<li><?php include 'part_auth_view.php'; ?></li>
             </ul>
         </nav>
