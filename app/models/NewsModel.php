b47fcb2dcef7856e0144e61a14068cbf7024d83b
solembum-php <solembum@i.ua>
2019-11-04 12:21:46 +0200

    create NewsModel.php

--- app/models/NewsModel.php
+++ app/models/NewsModel.php
@@ -5,14 +5,31 @@ namespace models;
 use core\AbstractModel;
 use mysqli;
 
+class NewsModel extends AbstractModel {
 
-class NewsModel extends AbstractModel{
-    
+    /**
+     * add new post
+     * 
+     * @param array $post
+     */
     public function addPost($post) {
-	$query = "insert into users values (null, '{$post['title']}','{$post['text']}')";
-	$this->db->query($query);
-	if ($this->db->errno) {
-	    die($this->db->error);
-	}
+        $query = "insert into posts values (null, '{$post['title']}','{$post['text']}',null)";
+        $this->db->query($query);
+        if ($this->db->errno) {
+            die($this->db->error);
+        }
     }
+
+    public function getAllPosts() {
+        $query = "select * from posts";
+        $this->db->query($query);
+        if ($query) {
+            $posts = $query->fetch_all(MYSQLI_ASSOC);
+            if ($this->db->errno) {
+                die($this->db->error);
+            }
+            return $posts;
+        }
+    }
+
 }
